de1c3c2c2c9a3a367775d88a858f39e102ffe2c6
Mira <mif05@mail.aub.edu>
2026-04-10 20:18:09 +0300

    added files

---
+++ Lexer.py
@@ -0,0 +1,118 @@
+import re #Python's regular expression engine (the heart of the lexer)
+import sys #to read command-line arguments
+from collections import namedtuple #creates a lightweight object for tokens
+
+Token = namedtuple("Token", ["type", "value", "line", "column"])
+
+TOKEN_SPECIFICATION = [
+    # Keywords
+    ("KEYWORD", r"\b(agent|tool|task|action|system|run|if|for|in|string|int|bool|list)\b"),
+
+    #Literals
+    ("NUM", r"\b(0|[1-9][0-9]*)\b"),
+    ("STRING_LITERAL", r'"([^"\\]|\\.)*"'),#[^...]->match anything but what's after ^, so " and \ are not allowed bec " would end 
+                                           #the string early while \ alone at the end is problematic
+    ("BOOL_LITERAL", r"\b(true|false)\b"),
+    
+    #Identifiers
+    ("ID", r"\b[a-zA-Z_][a-zA-Z_0-9]*"),
+
+    #Return Arrow
+    ("ARROW", r"->"),
+
+    #Operators
+    ("OPERATORS", r"\+|-|=|<|>|<=|>=|!=|==|\*|/"),
+
+    #Delimiters
+    ("DELIMITER", r"\(|\)|\{|\}|\[|\]|,|\.|:"),
+
+    #Comments
+    ("COMMENT", r"//.*|#.*")
+
+    #Whitespace
+    ("WHITESPACE", r"[ \t\r\n]+"),
+
+    #any other character
+    ("MISTMATCH", r".")
+]
+
+token_regex = re.compile("|".join(f"(?P<{name}>{pattern})" for name, pattern in TOKEN_SPECIFICATION))
+
+def tokenize(code):
+    tokens = [] #initialize token array
+    line_num = 1 #tracks which line we're currently at
+    line_start = 0 #tells me at which character position my current line begins at since the source code given is one single long string
+
+    #finditer():
+    # starts at position 0
+    # applies the regex
+    # finds a match
+    # advances the input pointer
+    # repeats until EOF
+
+    for match in token_regex.finditer(code): #iterates over every token match from left to right
+        # lastgroup	What kind of token is this? example: "KEYWORD"
+        # group()	What text was matched? ex: "agent"
+        # start()	Where did it occur?(which column in the line?)
+        kind = match.lastgroup
+        value = match.group() 
+        column = match.start() - line_start + 1 #match.start() gives the absolute character position in the whole string, so subtracting line_start 
+                                                #gives you the offset from the beginning of the current line. The +1 makes it 1-indexed.
+
+        #print (kind, value, column)  # Debug print to trace tokenization
+
+        #SKIP ANY EMPTY LINES OR COMMENTED ONES
+        if kind == "WHITESPACE":
+            if "\n" in value:
+                line_num += value.count("\n")
+                line_start = match.end()
+            continue
+
+        if kind == "COMMENT":
+            continue #since our comments' regex doesn't involve having more than one line commmented in one single comment
+
+        #display an error if nothing matched.
+        if kind == "MISMATCH":
+            raise SyntaxError(
+                f"Unexpected character {value!r} at line {line_num}, column {column}"
+            )
+
+        #else: add the token to the tokens array if it did match and we reached this part of the code
+        tokens.append(Token(kind, value, line_num, column))
+
+    return tokens
+
+
+def main():
+    if len(sys.argv) != 2: #assuming you run the program from the cmd line, sys.argv is a list of everything you typed there
+        print("Usage: python lexer.py <source_file>")#sys.argv[0] is always the script name, while sysargv[1] is always the src file
+        #so total length should be exactly 2, otherwise it's a wrong input -> tell the user how to run the program in an error msg
+        sys.exit(1)#1=program ended with error, 0=no error
+
+    filename = sys.argv[1]
+
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            code = f.read() #this is to denote the whole code in the file by "code" in this file
+    except FileNotFoundError:
+        print(f"Error: file '{filename}' not found.")
+        sys.exit(1)
+
+    try:
+        tokens = tokenize(code)
+    except SyntaxError as e:
+        print("Lexical error:", e)
+        sys.exit(1)
+
+    for token in tokens: #prints token type and value while padding them both to 15 chars wide to make it neat and easily readable
+        print(f"{token.type:<15} {token.value:<15} (line {token.line}, col {token.column})")
+
+
+if __name__ == "__main__":
+    main()
+
+#every python file has a built-in variable called __name__ and it depends on how the file is being used. 
+#initially, __name__ is "__main__" by defualt. 
+#if you're tryign to run the program form the file itself then __name__=="__main__"
+#but if you import this file to another one then __name__=filename now so it's !="__main__"
+#so main won't run here on its own if u try to run the program, unless u run the lexer file directly form the prompt
